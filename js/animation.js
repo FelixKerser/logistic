ee4211a1131c043c5868e9d8a18c476e9665b76a
FelixKerser <62158934+FelixKerser@users.noreply.github.com>
2022-09-11 23:53:13 +0200

    small fix animations

--- js/animation.js
+++ js/animation.js
@@ -19,9 +19,6 @@
               //Как только досчитали - стираем интервал.
               counter[i].innerText = counter[i].dataset.to;
               clearInterval(int);
-              document.getElementById('strange__truck').style.transform = 'translateX(0%)';
-              document.getElementById('mapa__img').style.transform = 'translateY(-0%)';
-              document.getElementById('green-mark').style.transform = 'translateY(200%)';
             }
           }, 60);
         }
@@ -30,34 +27,6 @@
 
 })();
 
-(function(){
-  let counter = document.querySelectorAll('.counter');
-  let limit = 0; // Переменная, чтобы останавливать функцию, когда всё запустится.
-  window.addEventListener('scroll', function(){  
-    if( limit == counter.length ){ return; }
-    
-    for(let i = 0; i < counter.length; i++){
-      let pos = counter[i].getBoundingClientRect().top; //Позиция блока, считая сверху окна
-      let win = window.innerHeight - 40; // На 40 пикселей меньше, чем высота окна
-      if( pos < win && counter[i].dataset.stop === "0" ){
-        counter[i].dataset.stop = 1; // Останавливаем перезапуск счета в этом блоке
-        let x = 0;
-        limit++; // Счетчик будет запущен, увеличиваем переменную на 1
-        let int = setInterval(function(){
-          // Раз в 60 миллисекунд будет прибавляться 50-я часть нужного числа
-          x = x + Math.ceil( counter[i].dataset.to / 60 ); 
-          counter[i].innerText = x;
-          if( x > counter[i].dataset.to ){
-            //Как только досчитали - стираем интервал.
-            counter[i].innerText = counter[i].dataset.to;
-            clearInterval(int);
-          }
-        }, 60);
-      }
-    }
-  });
-
-})();
 
 let mob_lang_open = document.getElementById('mob-lang-opener');
 let counter = 0;
@@ -73,8 +42,3 @@ let mob_lang_list_open = function(){
 }
 
 mob_lang_open.onclick = mob_lang_list_open;
-
-document.getElementById('big-yellow-mark__img').style.transform = 'translateY(-0%)';
-document.getElementById('big-car__img').style.transform = 'translateX(-0%)';
-document.getElementById('small-mark1__img').style.transform = 'translateY(-0%)';
-document.getElementById('small-mark2__img').style.transform = 'translateY(-0%)';
